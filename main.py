d3de6b39c6b55e32f4f05136a6fcb4b8cef22436
Hendrik Merz <67654824+rickaldo@users.noreply.github.com>
2021-06-29 17:58:21 +0200

    Path refresh

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import helper
 import visualization as vs
 
 paths = [
-    '/Users/ricked-corp/Documents/SortedNumbers/Januar2020.csv',
-    '/Users/ricked-corp/Documents/SortedNumbers/Februar2020.csv',
-    '/Users/ricked-corp/Documents/SortedNumbers/March2020.csv',
-    '/Users/ricked-corp/Documents/SortedNumbers/April2020.csv',
-    '/Users/ricked-corp/Documents/SortedNumbers/Mai2020.csv',
-    '/Users/ricked-corp/Documents/SortedNumbers/Juni2020.csv',
-    '/Users/ricked-corp/Documents/SortedNumbers/Juli2020.csv',
-    '/Users/ricked-corp/Documents/SortedNumbers/August2020.csv',
-    '/Users/ricked-corp/Documents/SortedNumbers/September2020.csv',
+    '/SortedNumbers/Januar2020.csv',
+    '/SortedNumbers/Februar2020.csv',
+    '/SortedNumbers/March2020.csv',
+    '/SortedNumbers/April2020.csv',
+    '/SortedNumbers/Mai2020.csv',
+    '/SortedNumbers/Juni2020.csv',
+    '/SortedNumbers/Juli2020.csv',
+    '/SortedNumbers/August2020.csv',
+    '/SortedNumbers/September2020.csv',
 ]
 
 helperdata = helper.Helper(paths).createLogicObjects()
